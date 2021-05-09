b52dddf86a28209568303b94eb7b016f2cc8aebc
sharefeel@gmail.com <sharefeel@gmail.com>
2021-05-09 23:16:47 +0900

    rename

--- src/main/java/org/example/hellorest/HelloRestApp.java
+++ src/main/java/org/example/hellorest/HelloRestApp.java
@@ -19,18 +19,6 @@ public class HelloRestApp {
         SpringApplication.run(HelloRestApp.class);
     }
 
-    @GetMapping("hello")
-    public ResponseEntity<String> hello() throws UnknownHostException {
-        String localAddress = InetAddress.getLocalHost().getHostAddress();
-        String localHostName = InetAddress.getLocalHost().getHostName();
-        return ResponseEntity.ok("HelloRest Ver 2. I am " + localHostName + "(" + localAddress + ") ");
-    }
-
-    @GetMapping("alive")
-    public ResponseEntity<String> alive() {
-        return ResponseEntity.ok("alive ");
-    }
-
     @GetMapping("")
     public ResponseEntity<String> root() {
         return ResponseEntity.ok("ok ");
